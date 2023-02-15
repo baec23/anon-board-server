967d2d467257a9bd6b5536925933941f0f544b81
Changmin Bae <baec23@gmail.com>
2023-02-15 15:10:10 +0900

    Cors policy update

--- src/main/kotlin/com/baec23/anonboard/WebMvcConfigurer.kt
+++ src/main/kotlin/com/baec23/anonboard/WebMvcConfigurer.kt
@@ -7,6 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebMvcConfigurer: WebMvcConfigurer{
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/anon-board-api/v1/post").allowedOrigins("http://localhost:5173")
+        registry.addMapping("/**").allowedMethods("*")
     }
 }
